953808cc37fea63329915c55e5d2827afe0705cf
Rohan Sahai <rsahai91@gmail.com>
2014-05-07 11:29:08 -0700

    inherits

---
+++ inherits.js
@@ -0,0 +1,25 @@
+Function.prototype.inherits = function(parent){
+  function Surrogate(){};
+  Surrogate.prototype = parent.prototype;
+  this.prototype = new Surrogate();
+};
+
+function Animal(name, age){
+  this.name = name;
+  this.age = age;
+};
+
+Animal.prototype.makeNoise = function(){
+  console.log("rawr");
+};
+
+function Dog(name, age, color){
+  Animal.call(this, name, age);
+  this.color = color;
+};
+
+Dog.inherits(Animal);
+
+var dog = new Dog("louie", "2", "blue");
+console.log(dog);
+dog.makeNoise();
