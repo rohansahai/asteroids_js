517654d0b218ca732ee9d1db85744391088bb673
Rohan Sahai <rsahai91@gmail.com>
2014-05-07 10:51:43 -0700

    through myBind

---
+++ arguments.js
@@ -0,0 +1,38 @@
+var sum = function() {
+  var sum = 0;
+  for(var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+};
+
+Function.prototype.myBind = function(object) {
+  var f = this;
+  //take off object; leave just the arguments
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  return function () {
+    var otherArgs = Array.prototype.slice.call(arguments);
+    f.apply(object, args.concat(otherArgs));
+  }
+};
+
+
+function myObject(name) {
+  this.name = name;
+};
+
+myObject.prototype.mySum = function() {
+  console.log(this.name);
+  var sum = 0;
+  for(var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  console.log(sum);
+  return sum;
+};
+
+var bob = new myObject("bob");
+//bob.mySum(1, 2);
+var myBoundFunction = bob.mySum.myBind(bob, 1, 2);
+myBoundFunction(3);
