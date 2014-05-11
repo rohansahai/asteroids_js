bc87186961acb562e659faa37393c5c61072d481
Rohan Sahai <rsahai91@gmail.com>
2014-05-11 02:42:49 -0700

    beginning rotation logic

--- asteroids/ship.js
+++ asteroids/ship.js
@@ -3,6 +3,7 @@
 
   var Ship = Asteroids.Ship = function(pos, vel, radius, color){
     Asteroids.MovingObject.call(this, pos, vel, radius, color);
+    this.rotation = 0;
   };
 
   Ship.RADIUS = 15;
@@ -10,10 +11,25 @@
 
   Ship.inherits(Asteroids.MovingObject);
 
+  Ship.degreesToRadians = function (degrees){
+    return (degrees * Math.PI)/180
+  }
+
+  //Adjacent * Tan(degrees for x coordinate) ?
+
+
   Ship.prototype.power = function(impulse) {
     this.vel[0] += impulse[0];
     this.vel[1] += impulse[1];
   };
+
+  Ship.prototype.rotate = function(dir) {
+    if (dir === "left"){
+      this.rotation -= 5;
+    } else {
+      this.rotation += 5;
+    }
+  }
 
   Ship.prototype.fireBullet = function() {
     var speed = Math.sqrt(Math.pow(this.vel[0],2) + Math.pow(this.vel[1], 2));
@@ -25,21 +41,22 @@
       "green"))
     }
   };
-  
+
+  Ship.prototype.move = function() {
+    this.pos[0] += this.vel[0] * Math.tan(Ship.degreesToRadians(this.rotation));
+    this.pos[1] += this.vel[1];
+  };
+
   Ship.prototype.draw = function(ctx) {
-	  
-		// var shipImage = new Image();
-// 		shipImage.src = 'banana.jpeg';
-// 		ctx.drawImage(shipImage, this.pos[0], this.pos[0], 50, 50);
-		
+
 		var shipImage = new Image();
 
 		shipImage.onload = function() {
 		  ctx.drawImage(shipImage, 250, 250);
 		};
 		shipImage.src = 'banana.jpeg';
-		
+
   };
-  
+
 
 })(this);
